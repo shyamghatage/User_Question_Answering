b53d8f575f9e75e4df6d966b594c2ae7468365e0
shyamghatage <ghatageshyam2004@gmail.com>
2025-08-05 16:54:06 +0530

    Delete user_question_answering directory

--- user_question_answering/qa_model.py
+++
@@ -1,9 +0,0 @@
-# qa_model.py
-from transformers import pipeline
-
-# Load the pre-trained QA pipeline
-qa_pipeline = pipeline("question-answering", model="bert-large-uncased-whole-word-masking-finetuned-squad")
-
-def get_answer(question, context):
-    result = qa_pipeline(question=question, context=context)
-    return result['answer']
